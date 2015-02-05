4e634eec166e48998b0d01576cd00c609d7b62d8
rabid-angel <hagen715@morris.umn.edu>
2015-02-05 13:17:11 -0600

    Changed findHeaviestPet test to expect an array of JSON objects and return the heaviest JSON object.

--- client/views/main/main.controller.spec.js
+++ client/views/main/main.controller.spec.js
@@ -22,8 +22,9 @@ describe('Testing controller: mainCtrl', function(){
 
 
     it('should return the weight of the heaviest pet from an array', function(){
-        var petArray = [];
-        expect(scope.findHeaviestPet(petArray)).toEqual(35);
+        var petArray = [{text: "Brutus", weight: 1}, {text: "Fido", weight: 35}, {text: "Fluffy", weight: 20}];
+        expect(scope.findHeaviestPet(petArray)).toEqual({text: "Fido", weight: 35});
     });
 
+    // test for tied weights, just return the first found.
 });
