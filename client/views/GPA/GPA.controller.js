07a9523bf92e691587a48b92269bc5bbc2b7ed8f
Aaron Lemmon <lemmo031@morris.umn.edu>
2015-02-06 16:09:55 -0600

    Got GPA page to show up.

--- client/views/GPA/GPA.controller.js
+++ client/views/GPA/GPA.controller.js
@@ -4,9 +4,69 @@
 
 'use strict';
 
-angular.module('appModule')
-    .controller('GPACtrl', function($scope){
+angular.module("appModule")
+    .controller('GPACtrl', function($scope, $http){
+        console.log("GPA controller loaded!");
 
+        $scope.textField = "";
+        $scope.weightField = "";
+        $scope.heaviestPet = {text: "No Pet Found", weight: -1} //findHeaviestPet([]); // Initializes this field to have a default message.
 
+        // Normally, data like this would be stored in a database, and this controller would issue an http:get request for it.
+        $scope.data = [];
+
+        $scope.getPets = function(){
+            $http.get('api/pets').success(function(pets) {
+                $scope.data = pets;
+                $scope.heaviestPet = $scope.findHeaviestPet($scope.data);
+            });
+
+        };
+
+        $scope.getPets();
+
+        $scope.addData = function(){
+            if($scope.isNotEmpty($scope.textField)&& $scope.isPositive($scope.weightField)) {
+                $http.post('api/pets', {text: $scope.textField, weight: $scope.weightField}).success(function(){
+                    $scope.getPets();
+                });
+                $scope.textField = "";
+                $scope.weightField = "";
+            }
+        };
+
+        $scope.removeData = function(index){
+            $http.delete('/api/pets/' + $scope.data[index]._id).success(function(){
+                $scope.getPets();
+            });
+        };
+
+        $scope.cat = function(str1, str2){
+            return str1 + str2;
+        };
+
+        $scope.itemsInList = function(){
+            return $scope.data.length;
+        };
+
+        $scope.findHeaviestPet = function(arrayOfPets){
+            //Handle case of empty array
+            var heaviest = {text: "No Pets Found", weight: -1}
+            for(var i = 0; i < arrayOfPets.length; i++){
+                var currentPet = arrayOfPets[i];
+                if (currentPet.weight > heaviest.weight) {
+                    heaviest = currentPet;
+                }
+            }
+            return heaviest;
+        };
+
+        $scope.isPositive = function(number){
+            return number > 0;
+        }
+
+        $scope.isNotEmpty = function(text){
+            return text != "";
+        }
 
     });
