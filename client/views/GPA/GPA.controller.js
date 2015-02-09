a75f5d6614d514cd8815086cc28dfa18c52e046b
rabid-angel <hagen715@morris.umn.edu>
2015-02-09 16:16:05 -0600

    Implemented letterToNum function

--- client/views/GPA/GPA.controller.js
+++ client/views/GPA/GPA.controller.js
@@ -104,6 +104,20 @@ angular.module("appModule")
             }
         }
 
-
-
+        // This assumes the argument is a capital letter.
+        $scope.letterToNum = function(letter){
+            letter = letter.toUpperCase();
+            switch(letter) {
+                case "A":
+                    return 4.0;
+                case "B":
+                    return 3.0;
+                case "C":
+                    return 2.0;
+                case "D":
+                    return 1.0;
+                default:
+                    return 0.0;
+            }
+        }
     });
